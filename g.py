126536cab263191f49faba5e258abe7c56867eaf
Khamidullaev_Khamidulla <Khamidullaev_007@mail.ru>
2021-11-12 19:14:09 +0500

    First Commit

---
+++ g.py
@@ -0,0 +1,12 @@
+from turtle import *
+
+def flevr():
+    for i in range(300):
+        circle(200-i,90)
+        left(90)
+        circle(200-i,90),
+        left(18)
+        speed(11)
+
+flevr()
+mainloop()
